136222d08fb3c0482dcb5a41ba237456bb2e1da5
Praneeth Reddy <praneeth.kotholla@gmail.com>
2026-01-29 12:22:24 +0530

    entrypoint file

---
+++ 06-Mysql/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /tmp/mysql_root_password.txt ]; then
+   PASSWORD=$(cat /tmp/mysql_root_password.txt)
+   echo "Accessed root password"
+else
+    echo "password file not found"
+    exit 1
+fi
+
+
+# making it as available in environment
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+
+exec /entrypoint.sh mysqld
