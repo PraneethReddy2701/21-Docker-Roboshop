e9c64574055b4d0315950c7f337418d8cc826db1
Praneeth Reddy <praneeth.kotholla@gmail.com>
2026-01-29 12:27:00 +0530

    Docker file

--- 06-Mysql/entrypoint.sh
+++ 06-Mysql/entrypoint.sh
@@ -13,3 +13,15 @@ fi
 export MYSQL_ROOT_PASSWORD=$PASSWORD
 
 exec /entrypoint.sh mysqld
+
+
+# #!/bin/bash
+
+# if [ ! -f /tmp/mysql_root_password.txt ]; then
+#   echo "Password file not found"
+#   exit 1
+# fi
+
+# export MYSQL_ROOT_PASSWORD_FILE=/tmp/mysql_root_password.txt
+
+# exec /entrypoint.sh mysqld
